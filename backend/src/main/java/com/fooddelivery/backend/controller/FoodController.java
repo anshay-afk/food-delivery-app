4a84d4425f994ea18390a25bac550c2306ffb4cb
Anshay Kapurwan <techtree2004@gmail.com>
2026-06-29 16:55:07 +0530

    Fix Food images

--- backend/src/main/java/com/fooddelivery/backend/controller/FoodController.java
+++ backend/src/main/java/com/fooddelivery/backend/controller/FoodController.java
@@ -37,8 +37,36 @@ return "Seeding disabled";
 @GetMapping("/reset")
 public String resetFoods(){
 
-return "Disabled";
+foodRepository.deleteAll();
+
+foodRepository.save(
+new Food(
+null,
+"Burger",
+250.0,
+"Fast Food",
+"https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg"
+));
+
+foodRepository.save(
+new Food(
+null,
+"Pizza",
+450.0,
+"Italian",
+"https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg"
+));
+
+foodRepository.save(
+new Food(
+null,
+"Momos",
+120.0,
+"Snacks",
+"https://images.pexels.com/photos/7363386/pexels-photo-7363386.jpeg"
+));
+
+return "Reset Done";
 
 }
-
 }
